ab1d173468fced4aa201c600f5b21a8794055c83
Harley Trung <harley@socialsci.com>
2011-10-21 18:04:52 -0400

    change config/application.rb to not include test unit because when created the app, i type --skip-unit-test instead of --skip-test-unit :/

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -44,5 +50,8 @@ module HipayMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # If deplayed to HEROKU:
+    # config.assets.initialize_on_precompile = false
   end
 end
