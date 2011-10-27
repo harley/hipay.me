e7c816489de163d2243752bbead26a7e12bfcdd7
Harley Trung <harley@socialsci.com>
2011-10-27 07:20:00 -0400

    forgot to indicate bucket name to use for s3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,7 +3,7 @@ app = Dragonfly[:images]
 
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
-app.configure_with(:heroku, 'my_bucket_name') if Rails.env.production?
+app.configure_with(:heroku, 'hipayme') if Rails.env.production?
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
 
